589643f563551613fed584b5dd2519c8bbf448b8
glenn <glenn@G-Dog.local>
2013-08-10 16:49:40 +1000

    mod1: Video save object model test.

--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -1,5 +1,14 @@
-require 'spec-helper'
+require 'spec_helper'
 
 describe Video do
-  
+
+  it "saves itself" do
+    video = Video.new(title: "Family Guy", 
+                      small_cover_url: "/tmp/family_guy.jpg",
+                      large_cover_url: "/tmp/family_guy_large.jpg",
+                      description: "Peter Griffin & Co")
+    video.save
+    Video.first.title.should == "Family Guy"
+  end
+
 end
