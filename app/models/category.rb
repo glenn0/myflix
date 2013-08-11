3a3e5799c92eff16c56b8cbcea4dead320198b76
glenn <glenn@G-Dog.local>
2013-08-11 12:00:43 +1000

    Category: recent_videos implemented front and back, with tests.

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,8 @@
 class Category < ActiveRecord::Base
   has_many :videos
+
+  def self.recent_videos(category)
+    # Select top 6 videos per category
+    category.videos.find(:all, order: "created_at DESC", limit: 6)
+  end
 end
--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -2,12 +2,36 @@ require 'spec_helper'
 
 describe Category do
 
-  it "saves itself" do
-    category = Category.new(name: "Drama")
-    category.save
-    Category.first.name.should == "Drama"
-  end
-
   it { should have_many(:videos) }
 
+  describe "recent_items" do
+    before(:each) do
+      @comedy = Category.create(name: 'Comedy')
+      @drama = Category.create(name: 'Drama')
+      @romance = Category.create(name: 'Romance')
+      @video1 = @drama.videos.create(title: 'Family Guy', description: "Lucky there's a family guy.", small_cover_url: '/tmp/family_guy.jpg', large_cover_url: '/tmp/family_guy_large.jpg')
+      @video2 = @comedy.videos.create(title: 'Monk', description: "A quirky detective solves crime.", small_cover_url: '/tmp/monk.jpg', large_cover_url: '/tmp/monk_large.jpg')
+      @video3 = @comedy.videos.create(title: 'Futurama', description: "Fry moves to the future to deliver pizza and more.", small_cover_url: '/tmp/futurama.jpg', large_cover_url: '/tmp/futurama_large.jpg')
+      @video4 = @comedy.videos.create(title: 'South Park', description: "Come on down to South Park and meet some friends of mine.", small_cover_url: '/tmp/south_park.jpg', large_cover_url: '/tmp/south_park_large.jpg')
+      @video5 = @comedy.videos.create(title: 'Breaking Bad', description: "Walter White teaches chemistry and cooks meth.", small_cover_url: '/tmp/futurama.jpg', large_cover_url: '/tmp/futurama_large.jpg')
+      @video6 = @comedy.videos.create(title: 'The Killing', description: "Murder mysteries in Seattle.", small_cover_url: '/tmp/futurama.jpg', large_cover_url: '/tmp/futurama_large.jpg')
+      @video7 = @comedy.videos.create(title: 'Boston Legal', description: "Law in Boston.", small_cover_url: '/tmp/futurama.jpg', large_cover_url: '/tmp/futurama_large.jpg')
+    end
+
+    it "returns an empty array when there are no videos in the category" do
+      expect(Category.recent_videos(@romance)).to match_array([])
+    end
+
+    it "returns an array of <6 videos when there are less than 6 videos in the category, ordered by created_at DESC" do
+      expect(Category.recent_videos(@drama)).to match_array([@video1])
+    end
+
+    it "returns an array of 6 videos when there are 6 videos in the category, ordered by created_at DESC" do
+      expect(Category.recent_videos(@comedy)).to match_array([@video7, @video6, @video5, @video4, @video3, @video2])
+    end
+    it "returns an array of 6 videos when there are more than 6 videos in the category, ordered by created_at DESC" do
+      @video8 = @comedy.videos.create(title: 'Wilfred', description: "Ryan talks to a dog.", small_cover_url: '/tmp/futurama.jpg', large_cover_url: '/tmp/futurama_large.jpg')
+      expect(Category.recent_videos(@comedy)).to match_array([@video8, @video7, @video6, @video5, @video4, @video3])
+    end
+  end
 end
