e2edde953455c481a804f9aff85fd33fd1c4c6be
glenn <glenn@G-Dog.local>
2013-08-13 17:40:28 +1000

    Session user authentication implemented.

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Myflix::Application.routes.draw do
+  
   get 'ui(/:action)', controller: 'ui'
-
-  get '/', to: 'pages#front'
   get 'home', to: 'pages#home'
+  get '/', to: 'pages#front'
   get 'register', to: 'users#new'
   get 'sign_in', to: 'sessions#new'
+  get 'sign_out', to: 'sessions#destroy'
 
   resources :videos, only: [:show] do
     collection do
@@ -15,4 +16,6 @@ Myflix::Application.routes.draw do
   resources :categories, only: [:show]
 
   resources :users, only: [:create]
+
+  resources :sessions, only: [:create]
 end
