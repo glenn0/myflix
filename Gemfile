710dfef41c6a284a9ab2a5cc02ba68c3a3fddc45
glenn0 <glenn0>
2013-08-18 16:32:18 +1000

    Started controller tests, but forgot to branch. Continuing on Mod3 branch.

--- Gemfile
+++ Gemfile
@@ -6,6 +6,8 @@ gem 'bootstrap-sass'
 gem 'jquery-rails'
 gem 'bcrypt-ruby', '~> 3.0.0'
 gem 'bootstrap_forms'
+gem 'fabrication'
+gem 'faker'
 
 group :assets do
   gem 'sass-rails',   '~> 3.2.3'
---
+++ spec/controllers/videos_controller_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+
+describe VideosController do
+  describe "GET show" do
+    context "with authenticated users" do
+      before do
+        session[:user_id] = Fabricate(:user).id
+      end
+
+      it "assigns the requested video to @video" do
+        video = Fabricate(:video)
+
+        get :show, id: video.id
+        expect(assigns(:video)).to eq video
+      end
+
+      it "renders the :show template"
+    end
+  end
+end
