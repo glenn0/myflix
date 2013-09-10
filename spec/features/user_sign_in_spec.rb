10058ef7c5465b6b63f27918e22f98a9eb33ec12
glenn0 <glenn0>
2013-09-10 15:19:23 +1000

    Adding feature specs for login and my queue.

--- spec/features/user_sign_in_spec.rb
+++ spec/features/user_sign_in_spec.rb
@@ -1,11 +1,9 @@
-require 'spec'
+require 'spec_helper'
 
 feature "user signs in" do
   scenario "with valid email address and password" do
     bob = Fabricate(:user)
-    visit sign_in_path
-    fill_in "Email Address", with: bob.email
-    fill_in "Password", with: bob.password
-    click_button
+    sign_in(bob)
+    page.should have_content bob.full_name.split.first
   end
 end
