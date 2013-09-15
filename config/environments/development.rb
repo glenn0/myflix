64f4657d6d24f58784a77ecc0d8b06d31a79a572
glenn0 <glenn0>
2013-09-15 22:27:45 +1000

    Mail setting tweaks.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,7 +15,16 @@ Myflix::Application.configure do
 
   # Don't care if the mailer can't send
   config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.delver_method = :letter_opener 
+  #config.action_mailer.delivery_method = :letter_opener 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+  address:              'smtp.gmail.com',
+  port:                 587,
+  domain:               'gmail.com',
+  user_name:            ENV['GMAIL_UNAME'],
+  password:             ENV['GMAIL_PWORD'],
+  authentication:       'plain',
+  enable_starttls_auto: true  }
 
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
