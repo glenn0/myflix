8f5e011d43a734973f6f4743a84ab8bb1ca8b246
glenn0 <glenn0>
2013-10-03 17:29:39 +1000

    Fixing error in prod deployment error.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,7 +67,7 @@ Myflix::Application.configure do
   :address        => ENV['MAILGUN_SMTP_SERVER'],
   :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
   :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-  :domain         => 'yourapp.heroku.com',
+  :domain         => 'pure-hollows-4541.herokuapp.com',
   :authentication => :plain,
   }
   ActionMailer::Base.delivery_method = :smtp
